a0f0bf1a4bab299eb5361080caa02df652fbaf76
leoaleluia <aleluiasp17@hotmail.com>
2025-10-01 08:58:25 -0300

    estudos

---
+++ Modulo_D_Condições/Aula11.js
@@ -0,0 +1,9 @@
+console.log('O console funcionou')
+var vel = 50
+console.log(`O carro corre a ${vel}`)
+
+if (vel > 60){
+    console.log(`a velocidade do carro é ${vel}`)
+}else{
+    console.log(`A vel é ${vel}`)
+}
