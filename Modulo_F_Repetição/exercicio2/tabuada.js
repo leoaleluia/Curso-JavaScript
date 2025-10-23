3c1c270be83d8c3b2589c4ebf8b59731e68fb889
leoaleluia <aleluiasp17@hotmail.com>
2025-10-22 21:12:50 -0300

    projeto Banco

--- Modulo_F_Repetição/exercicio2/tabuada.js
+++ Modulo_F_Repetição/exercicio2/tabuada.js
@@ -4,12 +4,12 @@ function Gerar(){
     var txTtabuada = document.getElementById('txTtabuada')
     if (txTnum.value.length == 0){
         alert('Digite algo')
- 
+        
 
 
         
        // var item = document.createElement('option')/*nessas tres proximas linhas, temos as amostragem dos dados de forma dinamica, precisamos criar um item option para poder adcionar no mostrador*/
-        //item.text = txTnum.value /*O option recebe o item digitado no txTnum, mas poderia ser qualquer outro valor, no exemo a cima veremos a forma dinamica de contagem*/ 
+        //item.text = txTnum.value /*O option recebe o item digitado no txTnum, mas poderia ser qualquer outro valor, no exemplo a cima veremos a forma dinamica de contagem*/ 
         //txTtabuada.appendChild(item)/*a tabuada recebe o item. */
     }
 
--- Modulo_F_Repetição/tempCodeRunnerFile.js
+++ Modulo_F_Repetição/tempCodeRunnerFile.js
@@ -1,5 +1,9 @@
-var n1 = 1
-while (n1 < 5){
-    console.log(n1)
-    n1++
+var tot = 240
+var nota100 = 0
+var nota20 = 0
+console.log(nota20)
+while(tot >= 100){
+    tot -= 100
+    nota100++
 }
+console.log(nota100)
